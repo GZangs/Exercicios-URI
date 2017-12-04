b5c4cba171e213c621581a8df38ed6a3f91e25a4
Gabriel <Gabriel@192.168.1.8>
2017-12-03 22:41:12 -0200

    Commit inicial

---
+++ test/Uri1142/PumTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Uri1142;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PumTest {
+    
+    public PumTest() {
+    }
+
+    /**
+     * Test of gerarSequencia method, of class Pum.
+     */
+    @Test
+    public void testGerarSequencia() {
+    }
+    
+}
